6fdb8b3031253ed1c8c3077a0f56fdeb52364439
adoumadje <adoumadjecaleb@gmail.com>
2025-08-27 09:22:45 +0200

    updated

--- src/main/resources/upgrade.bat
+++ src/main/resources/upgrade.bat
@@ -1,1 +1,1 @@
-liquibase --driver=com.mysql.jdbc.Driver --classpath="c:\Program Files\MySQL\ConnectorJ\mysql-connector-java-5.1.41-bin.jar" --changeLogFile=db-changelog.xml --url="jdbc:mysql://localhost:3306/dating" --username=webuser --password=aSecretPass update
+liquibase --driver=com.mysql.jdbc.Driver --classpath="C:\Program Files\MySQL\mysql-connector-j-9.4.0\mysql-connector-j-9.4.0.jar" --changeLogFile=db-changelog.xml --url="jdbc:mysql://localhost:3306/dating" --username=webuser --password=aSecretPass update
