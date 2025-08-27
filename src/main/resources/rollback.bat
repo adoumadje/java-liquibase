2007b0f38409ed1bd6808e8f74aa2b0978190236
adoumadje <adoumadjecaleb@gmail.com>
2025-08-27 10:13:00 +0200

    rollback added

---
+++ src/main/resources/rollback.bat
@@ -0,0 +1,1 @@
+liquibase --driver=com.mysql.jdbc.Driver --classpath="C:\Program Files\MySQL\mysql-connector-j-9.4.0\mysql-connector-j-9.4.0.jar" --changeLogFile=db-changelog.xml --url="jdbc:mysql://localhost:3306/dating" --username=webuser --password=aSecretPass rollbackCount 1
